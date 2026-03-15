45705c73e66de274c126310caed487689706c727
nazi sharifi <banonazisharifi@gmail.com>
2026-03-15 15:24:49 +0430

    add new message

--- database/migrations/2026_03_15_105201_create_books_table.php
+++ database/migrations/2026_03_15_105201_create_books_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
-           
+             $table->string("name");
+            
             $table->timestamps();
         });
     }
