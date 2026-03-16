b5b5f9fa5940ec200c60864c3ee4c5f1632cb57e
nazi sharifi <banonazisharifi@gmail.com>
2026-03-16 15:36:08 +0430

    add new component

--- resources/views/components/⚡navbar.blade.php
+++ resources/views/components/⚡navbar.blade.php
@@ -9,5 +9,6 @@ new class extends Component
 ?>
 
 <div class=" p-5 flex justify-between items-center border-b border-gray-300" >
+    <h1>Hi there , Iam  </h1>
     {{-- Nothing worth having comes easy. - Theodore Roosevelt --}}
 </div>
