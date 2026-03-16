e73dca0875c34c5ce06cf9a7431317fc3234a011
nazi sharifi <banonazisharifi@gmail.com>
2026-03-16 15:40:58 +0430

    add new component

--- resources/views/components/⚡navbar.blade.php
+++ resources/views/components/⚡navbar.blade.php
@@ -10,6 +10,6 @@ new class extends Component
 
 <div class=" p-5 flex justify-between items-center border-b border-gray-300" >
     <h1 class="text-3xl font-serif text-gray-300">Hi there , Iam {{ $myName }} </h1>
-    <input type="text" class=" border border-gray-300 rounded-md py-1.5" />
+    <input type="text" class=" border border-gray-300 rounded-md py-1.5 outline-0 px-3 text-gray-300" />
     {{-- Nothing worth having comes easy. - Theodore Roosevelt --}}
 </div>
