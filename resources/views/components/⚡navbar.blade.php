9556e1b54b8d014db7b86b88b43bf46eccc458d1
nazi sharifi <banonazisharifi@gmail.com>
2026-03-16 15:35:18 +0430

    add new component

--- resources/views/components/⚡navbar.blade.php
+++ resources/views/components/⚡navbar.blade.php
@@ -8,6 +8,6 @@ new class extends Component
 };
 ?>
 
-<div class=" p-5 flex " >
+<div class=" p-5 flex justify-between items-center border-b border-gray-300" >
     {{-- Nothing worth having comes easy. - Theodore Roosevelt --}}
 </div>
