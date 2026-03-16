b7cbcda1700cd72d4c3ca090ce2cb6402c427818
nazi sharifi <banonazisharifi@gmail.com>
2026-03-16 16:52:38 +0430

    add new message

--- resources/views/components/⚡book-list.blade.php
+++ resources/views/components/⚡book-list.blade.php
@@ -8,7 +8,7 @@ new class extends Component
 };
 ?>
 
-<div class="w-full  flex justify-center items-center">
+<div class="w-full  flex justify-center items-center text-3xl font-bold">
     {{-- Simplicity is the essence of happiness. - Cedric Bledsoe --}}
     <h1>Hi</h1>
 </div>
