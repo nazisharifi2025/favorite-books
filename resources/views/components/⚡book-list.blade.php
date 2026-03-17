7f9a6fffa0e0e495cf144d719106966bcf99160a
nazi sharifi <banonazisharifi@gmail.com>
2026-03-17 15:53:57 +0430

    add style for navbar

--- resources/views/components/⚡book-list.blade.php
+++ resources/views/components/⚡book-list.blade.php
@@ -8,7 +8,6 @@ new class extends Component
 };
 ?>
 
-<div class="w-full  flex justify-center items-center text-3xl font-bold text-gray-300 text-center">
-    {{-- Simplicity is the essence of happiness. - Cedric Bledsoe --}}
-    <h1>Hi</h1>
+<div class="w-6xl grid grid-cols-1 md:grid-cols-2 justify-center items-center gap-4 text-gray-300 text-center">
+   
 </div>
