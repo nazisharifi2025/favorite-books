6b5e55b632b087009f97331022215752d9badeca
nazi sharifi <banonazisharifi@gmail.com>
2026-03-17 15:42:00 +0430

    add style for navbar

--- resources/views/components/⚡navbar.blade.php
+++ resources/views/components/⚡navbar.blade.php
@@ -23,8 +23,8 @@ new class extends Component
 };
 ?>
 
-<div class=" p-5 flex justify-between items-center border-b border-gray-500" >
-    <h1 class="text-3xl font-serif text-gray-300">Hi there , {{ $myName }} {{ $greeting }}</h1>
-    <input type="text" class=" border border-gray-300 rounded-md py-1.5 outline-0 px-3 text-gray-300" />
+<div class=" p-5 flex justify-between items-center  w-6xl backdrop-blur-2xl mx-auto" >
+    <h1 class="text-3xl font-serif text-blue-400">Hi there , {{ $myName }} {{ $greeting }}</h1>
+    <input type="text" placeholder="Enter your name" class=" border border-gray-300 rounded-md py-1.5 outline-0 px-3 text-gray-300" />
     {{-- Nothing worth having comes easy. - Theodore Roosevelt --}}
 </div>
