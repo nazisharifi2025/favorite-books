de5e2a5cfd11a06fdc909c0493e6fbb551b91ff5
nazi sharifi <banonazisharifi@gmail.com>
2026-03-17 15:52:43 +0430

    add style for navbar

--- resources/views/components/⚡navbar.blade.php
+++ resources/views/components/⚡navbar.blade.php
@@ -7,6 +7,10 @@ new class extends Component
     public $myName = "Nazi";
     public $date = 0;
     public $greeting = "" ;
+
+    public function getValue($value){
+        $this->myName = $value ;
+    }
     public function mount(){
         date_default_timezone_set("Asia/Kabul");
         $this->date = date("H");
@@ -23,8 +27,8 @@ new class extends Component
 };
 ?>
 
-<div class=" p-5 flex justify-between items-center  w-6xl backdrop-blur-2xl mx-auto bg-gray-500 rounded-b-md" >
+<div class=" p-5 flex justify-between items-center  w-6xl backdrop-blur-2xl mx-auto bg-gray-500 rounded-b" >
     <h1 class="text-3xl font-serif text-blue-400">Hi there , {{ $myName }} {{ $greeting }}</h1>
-    <input type="text" placeholder="Enter your name" class=" border border-gray-300 rounded-md py-1.5 outline-0 px-3 text-gray-300" />
+    <input wire:input="getValue($event.target.value)" type="text" placeholder="Enter your name" class="ring ring-blue-400 rounded-md py-1.5 outline-0 px-3 text-gray-300" />
     {{-- Nothing worth having comes easy. - Theodore Roosevelt --}}
 </div>
