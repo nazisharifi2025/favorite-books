a768e758378398c6f944541b768f67c2eb746756
nazi sharifi <banonazisharifi@gmail.com>
2026-03-17 16:18:56 +0430

    add book list componnit

--- resources/views/components/⚡book-list.blade.php
+++ resources/views/components/⚡book-list.blade.php
@@ -13,8 +13,8 @@ new class extends Component
 
 <div class="w-6xl grid grid-cols-1 md:grid-cols-2 justify-center items-center gap-4 text-gray-300 mx-auto my-6">
    @foreach ($books as $book)
-       <div class=" bg-gray-600  p-4 flex flex-col gap-2 rounded-md">
-        <div class="bg-linear-60 from-blue-200 to-blue-700 text-white ">
+       <div class=" bg-gray-600  p-4 flex flex-col gap-2 rounded-md backdrop-blur-3xl">
+        <div class="bg-linear-60 from-blue-200 to-blue-700 text-white rounded-md ">
             <i class="fa-solid fas-book-open"></i>
         </div>
         <h1 class="text-gray-400 text-2xl ">{{ $book->name }}</h1>
