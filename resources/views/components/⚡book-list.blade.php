b754c16f54625df1e62c5b88012e47a3ba01b766
nazi sharifi <banonazisharifi@gmail.com>
2026-03-17 16:08:36 +0430

    add book list componnit

--- resources/views/components/⚡book-list.blade.php
+++ resources/views/components/⚡book-list.blade.php
@@ -1,13 +1,22 @@
 <?php
 
 use Livewire\Component;
-
+use App\Models\Book ;
 new class extends Component
 {
-    //
+    public $books ;
+    public function mount(){
+        $this->books = Book::all();
+    }
 };
 ?>
 
-<div class="w-6xl grid grid-cols-1 md:grid-cols-2 justify-center items-center gap-4 text-gray-300 text-center">
-   
+<div class="w-6xl grid grid-cols-1 md:grid-cols-2 justify-center items-center gap-4 text-gray-300 mx-auto my-6">
+   @foreach ($books as $book)
+       <h1>{{ $book->name }}</h1>
+       <h2>Author: {{ $book->Author }} </h2>
+       <div>
+        
+       </div>
+   @endforeach
 </div>
