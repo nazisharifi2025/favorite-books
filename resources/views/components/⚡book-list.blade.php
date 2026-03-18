1ca178ec7dbba3ca7aad6ee80d3fbf14e5e4c688
nazi sharifi <banonazisharifi@gmail.com>
2026-03-18 15:18:35 +0430

    add modal for delete button

--- resources/views/components/⚡book-list.blade.php
+++ resources/views/components/⚡book-list.blade.php
@@ -36,7 +36,7 @@ new class extends Component
    @endforeach
    @if ($isModel)
        <div class="w-full min-h-screen absolute top-0 right-0 flex justify-center items-center ">
-       <div class=" bg-white rounded-md ">
+       <div class=" bg-white rounded-md border-gray-200 flex ">
        </div>
    @endif
 </div>
